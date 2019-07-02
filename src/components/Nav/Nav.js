e86cb4b79221dc84066496c70b511d1c7fc64d5b
unknown <kpnba90@gmail.com>
2019-07-02 13:27:42 -0400

    did nav bar need to add more things and style

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import TokenService from '../../services/token-service';
 import UserContext from '../../contexts/UserContext';
+import './Nav.css';
 
 class Nav extends Component {
   static contextType = UserContext
