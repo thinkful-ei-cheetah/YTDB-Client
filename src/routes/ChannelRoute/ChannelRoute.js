99474e22648240d03fbe18bea8d4a61930b2870a
rahmor <rahmor@yahoo.com>
2019-07-03 10:33:09 -0400

    add static channel component

--- src/routes/ChannelRoute/ChannelRoute.js
+++ src/routes/ChannelRoute/ChannelRoute.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import STORE from './ChannelDummy';
+import Channel from '../../components/Channel/Channel';
 
 class ChannelRoute extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div>Channel Route</div>
-    )
+      <>
+        <Channel channel={STORE} />
+      </>
+    );
   }
 }
 
