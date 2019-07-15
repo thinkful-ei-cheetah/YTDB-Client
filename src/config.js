3769cab90ea8c6c8d3bcf97c0d58514b1436031d
Jonathan Lassen <jonathan.lassen@gmail.com>
2019-07-15 18:14:01 -0400

    cypress testing setup

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://ytdbapi.herokuapp.com/api',
   REACT_APP_YTAPI: process.env.REACT_APP_YTAPI,
   TOKEN_KEY: 'ytdb-client-auth-token',
 }
